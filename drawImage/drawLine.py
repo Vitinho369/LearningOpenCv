eee601ab66adb3cc67e1a4cbc382eff4ff563634
Vitinho369 <73426079+Vitinho369@users.noreply.github.com>
2023-03-21 09:29:31 -0300

    Delete drawImage directory

--- drawImage/drawLine.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import utils
-
-img = cv.imread('./Python/LearningOpenCv/lena.jpg')
-
-#Imagem, coordenadas de inicio, coordenadas de fim, cor, expessura 
-cv.line(img, (0,0), (150,150), (255,255,255), 2)
-
-utils.imgShow(img)
--- drawImage/utils.py
+++
@@ -1,6 +0,0 @@
-import cv2 as cv
-
-def imgShow(img):
-    cv.imshow('image', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
